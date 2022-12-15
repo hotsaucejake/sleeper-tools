df643d7b34f55dbb1dbcdac566cad03a1df36626
hotsaucejake <get@hotsaucejake.com>
2022-12-15 13:49:06 -0500

    shoulda coulda woulda initial route

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ShouldaCouldaWoulda\SelectLeagueController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('shoulda-coulda-woulda', SelectLeagueController::class)->name('shoulda-coulda-woulda.select-league');
