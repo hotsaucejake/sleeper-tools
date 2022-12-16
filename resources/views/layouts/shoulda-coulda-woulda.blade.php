5c4e0d87d7cc151944c2329c535cf142b57e81a1
hotsaucejake <get@hotsaucejake.com>
2022-12-16 08:00:12 -0500

    update title

--- resources/views/layouts/shoulda-coulda-woulda.blade.php
+++ resources/views/layouts/shoulda-coulda-woulda.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="description" content="">
     <meta name="author" content="hotsaucejake">
-    <title>Laravel Tools</title>
+    <title>Sleeper Tools by hotsaucejake</title>
 
     @vite(['resources/js/app.js'])
 
